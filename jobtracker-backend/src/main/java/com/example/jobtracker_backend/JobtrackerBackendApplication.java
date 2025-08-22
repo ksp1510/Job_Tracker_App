3dd50d443de1fdd5b070a563d92aaead21639110
Kishan <ksp1510@gmail.com>
2025-08-21 22:36:16 -0400

    Add backend (Spring Boot) and frontend (Angular)

---
+++ jobtracker-backend/src/main/java/com/example/jobtracker_backend/JobtrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobtracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobtrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobtrackerBackendApplication.class, args);
+	}
+
+}
---
+++ jobtracker-backend/src/test/java/com/example/jobtracker_backend/JobtrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobtracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobtrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
